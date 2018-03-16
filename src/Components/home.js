8477425f336f4bbaecfed6066f953278bc1349ac
Gavin Dibley <elantri@gmail.com>
2018-03-17 02:45:38 +1100

    Change design to polish up the home screen

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,31 +1,100 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity
+} from "react-native";
+
+import { Asset, Audio } from "expo";
 export default class Home extends React.Component {
   render() {
+    const { sound, status } = Audio.Sound.create(
+      require("./../music/clockwork_clowns.mp3"),
+      {
+        shouldPlay: true,
+        rate: 1.0,
+        shouldCorrectPitch: true,
+        volume: 1.0,
+        isMuted: false,
+        isLooping: true
+        // // UNCOMMENT THIS TO TEST THE OLD androidImplementation:
+        // androidImplementation: 'MediaPlayer',
+      }
+    );
+
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image
+          style={{
+            backgroundColor: "#e67e22",
+            flex: 1,
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            justifyContent: "center"
+          }}
+          source={require("./../images/home-bg.jpg")}
+        />
+        <View style={{ flex: 1 }} />
         <View style={styles.header}>
           <Image
             source={require("./../images/logo.png")}
             style={[styles.logo]}
           />
         </View>
-        <View style={{ flex: 2, backgroundColor: "skyblue" }}>
-          <Text>Herd be game</Text>
+        <View style={styles.bgBar} />
+        <View style={styles.buttonBar}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => Alert.alert("Start Game")}
+          >
+            <Text style={styles.buttonText}>Play</Text>
+          </TouchableOpacity>
         </View>
-        <View style={{ flex: 3, backgroundColor: "steelblue" }} />
-        {/* <Button  style={styles.button} title="Play" onPress={() => Alert.alert("Start Game")} /> */}
+        <View style={styles.bottom} />
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
   header: {
     flex: 1,
-    backgroundColor: "#fff",
+
     alignItems: "center",
     justifyContent: "center"
   },
+  bgBar: {
+    flex: 7
+  },
+  buttonBar: {
+    flex: 2,
+    alignItems: "center"
+  },
   logo: {},
-  button: {}
+  bottom: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#2980b9",
+    padding: 10,
+    width: 200,
+    borderRadius: 15
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 30
+  }
 });
